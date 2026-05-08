92459e5220735e345bc6e8f760774097e3ab2534
PintoJR <guimoraespinto@gmail.com>
2026-05-08 19:20:06 -0300

    Lista de Restaurantes 0_2

--- src/components/ProductListPerfil/styles.ts
+++ src/components/ProductListPerfil/styles.ts
@@ -15,35 +15,5 @@ export const List = styled.ul`
 `
 
 export const Title = styled.h2`
-  font-size: 18px;
   font-weight: bold;
 `
-
-export const Modal = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 100;
-  display: none;
-
-  &.visivel {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.8);
-  }
-`
-
-export const ModalContent = styled.div``
-
-export const ButtonModal = styled.div``
